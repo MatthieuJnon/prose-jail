32265270912006702e0895921bcd9930be0d458b
Matthieu Joannon <mjoannon@methys.com>
2018-05-06 21:19:48 +0200

    WIP

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'La prison de TAGMP2 ??' });
 });
+
+
+router.post('/', function (req, res, next) {
+  console.log("received post request");
+  // TODO: faire cette fonction.
+});
+
 
 module.exports = router;
