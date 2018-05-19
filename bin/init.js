79121524571ae7c7e51435912ce653124b3a2b81
MatthieuJnon <mattjoannon@gmail.com>
2018-05-19 17:32:11 +0200

    init db script now with promises

--- bin/init.js
+++ bin/init.js
@@ -9,33 +9,45 @@ const connection = mysql.createConnection({
     password: configMysql.password
 });
 
-connection.query('CREATE DATABASE IF NOT EXISTS '+ configMysql.database, function(err) {
+connection.query('CREATE DATABASE IF NOT EXISTS ' + configMysql.database, function (err) {
     if (err) throw err;
     connection.query('USE ' + configMysql.database, function (err) {
         if (err) throw err;
         connection.query('CREATE TABLE IF NOT EXISTS bolosses('
-        + 'id INT NOT NULL AUTO_INCREMENT,'
-        + 'PRIMARY KEY(id)'
-        + 'name VARCHAR(30)'
-        + 'position VARCHAR(10)'
-        + ')', function (err) {
-            if (err) throw err;
-        });
+            + 'id INT NOT NULL AUTO_INCREMENT, '
+            + 'PRIMARY KEY(id), '
+            + 'name VARCHAR(30), '
+            + 'position VARCHAR(10)'
+            + ')', function (err) {
+                if (err) throw err;
+                insertAllBolosses().then(exit);
+            });
     });
 });
 
 const lesGens = [
-    {name : "mosca", position: "000000"},
-    {name : "leroy", position: "000000"},
-    {name : "matthieu", position: "000000"},
-    {name : "greg", position: "000000"},
-    {name : "pavlo", position: "000000"},
-    {name : "legrand", position: "000000"},
-    {name : "mehdi", position: "000000"},
-];  
+    { name: "mosca", position: "000000" },
+    { name: "leroy", position: "000000" },
+    { name: "matthieu", position: "000000" },
+    { name: "greg", position: "000000" },
+    { name: "pavlo", position: "000000" },
+    { name: "legrand", position: "000000" },
+    { name: "mehdi", position: "000000" }
+];
 
-lesGens.forEach(function(unMec) {
-    connection.query('INSERT INTO bolosses SET ?', unMec, function(err){
-        if (err) throw err;
+function insertAllBolosses() {
+    return Promise.all(lesGens.map(insertOneBolosse));
+}
+
+function insertOneBolosse(bolosse) {
+    return new Promise(function (resolve, reject) {
+        connection.query('INSERT INTO bolosses SET ?', bolosse, function (err) {
+            if (err) throw err;
+            resolve();
+        });
     });
-});
+}
+
+function exit() {
+    process.exit();
+}
